48eec9c2547b090c5dfd08772a9ecdc1ef7e737b
r3nanp <renanmol87@gmail.com>
2020-10-19 23:28:09 -0300

    feat (styles): add global style and theming

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: var(--bg);
+    color: var(--text);
+    font: 400 16px Roboto, sans-serif;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  :root {
+    --bg:  ${props => props.theme.colors.background};
+    --text: ${props => props.theme.colors.text};
+    --primary: ${props => props.theme.colors.primary};
+  }
+`
---
+++ src/styles/main.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const List = styled.ul`
+  padding-left: 20px;
+  color: var(--primary);
+
+  > li {
+    margin-bottom: 20px;
+  }
+`
