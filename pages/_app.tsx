e70bc52c9f6dff97d806174cd01ad010c8bbf87f
r3nanp <renanmol87@gmail.com>
2020-10-19 23:27:06 -0300

    refactor (pages): move the folder to src

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/react-in-jsx-scope */
-
-import { AppProps } from 'next/app'
-
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
