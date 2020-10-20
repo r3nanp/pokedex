1f70ccf7d209b04a5ccabe331db24b2fd232fbc5
r3nanp <renanmol87@gmail.com>
2020-10-20 12:26:41 -0300

    feat (link): add another page

---
+++ src/pages/link.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/react-in-jsx-scope */
+import Head from 'next/head'
+
+const Link: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Link</title>
+      </Head>
+
+      <main>
+        <h1>teste</h1>
+      </main>
+    </>
+  )
+}
+
+export default Link
