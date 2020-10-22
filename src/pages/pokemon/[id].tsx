f4a6ea1271c0018fffbea9b2610c9c5e86dbd039
r3nanp <renanmol87@gmail.com>
2020-10-22 01:06:06 -0300

    fix (id): ssg working

---
+++ src/pages/pokemon/[id].tsx
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable camelcase */
+import Head from 'next/head'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+
+export default function Pokemon({ info }): JSX.Element {
+  const { isFallback } = useRouter()
+
+  if (isFallback) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Pokémon Detail</title>
+      </Head>
+
+      <main>
+        <h1>{info.species.name}</h1>
+
+        <img
+          src={info.sprites.front_default}
+          alt={info.species.name}
+          width="100"
+          style={{
+            borderRadius: '50%',
+          }}
+        />
+      </main>
+    </div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const request = await fetch('https://pokeapi.co/api/v2/pokedex/2')
+
+  const pokemons = await request.json().then(req => {
+    return req.pokemon_entries
+  })
+
+  const paths = pokemons.map(pokemon => {
+    return {
+      params: {
+        id: pokemon.entry_number.toString(),
+      },
+    }
+  })
+
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params
+
+  const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+  const data = await request.json()
+
+  return {
+    props: {
+      info: data,
+    },
+    revalidate: 10,
+  }
+}
