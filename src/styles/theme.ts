eccc64e6f8ea8bbad176682fabaaa965d7d81d95
r3nanp <renanmol87@gmail.com>
2020-10-22 15:28:01 -0300

    style: add the gray color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,10 @@ const theme = {
     background: '#121214',
     text: '#e1e1e6',
     primary: '#8257e6',
-    'primary-hover': '#5C2EB8'
+    'primary-hover': '#5C2EB8',
+    'gray-600': '#29292e',
+    'gray-700': '#202024',
+    'gray-800': '#121214',
   }
 }
 
