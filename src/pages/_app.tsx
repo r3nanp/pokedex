aebf09a127cd076ebe8f3994f0be720e52110f81
r3nanp <renanmol87@gmail.com>
2020-11-11 12:17:23 -0300

    refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@
 
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
-
-import theme from '../styles/theme'
 import GlobalStyles from '../styles/global'
+
+import { theme } from '../styles/theme'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
