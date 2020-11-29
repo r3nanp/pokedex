3bf702f063d5e08d7a6bcd8e139f269d883c3d6e
r3nanp <renanmol87@gmail.com>
2020-11-29 20:56:41 -0300

    feat: add memo hooks

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { memo } from 'react'
 import { Container } from './styles'
 
 const PokemonCard: React.FC = ({ children }) => {
   return <Container whileHover={{ scale: 1.1 }}>{children}</Container>
 }
 
-export default PokemonCard
+export default memo(PokemonCard)
